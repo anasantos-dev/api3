9d5c3ed970a3dd400e3452ce6ab8f91de2a13403
anasantos-dev <acws_ss@hotmail.com>
2024-08-26 18:09:04 -0300

    feat:

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-
-
 const express = require("express");   
 const router = express.Router();
 const cors = require("cors");
@@ -9,28 +7,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const users = []
+const foods = []
 
-const getUsers = router.get('/users',(req, res)=>{
- res.status(200).json(users)
+const getfoods = router.get('/foods',(req, res)=>{
+ res.status(200).json(foods)
 
 })
 
-const createUser = router.post('/users',(req, res)=>{
-    const user = {
+const createfoods= router.post('/foods/creat',(req, res)=>{
+    const food = {
     id: req.body.id,
     title: req.body.title,
     description: req.body.description,
-    status: req.body.status('true')
+    status: req.body.status
     }
 
-users.push(user);
+    foods.push(food);
+    
+    res.status(201).json({ message: `food ${food.title} criado com sucesso!` })
+})
 
 
-    })
-
-app.use(createUser)
-app.use(getUsers)
+app.use(createfoods)
+app.use(getfoods)
 
 app.listen(3333, console.log("Servidor rodando com sucesso"));
 
