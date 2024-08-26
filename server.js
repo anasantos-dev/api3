a8ff523ada457738edd1a62034f9b4cd32db4c67
anasantos-dev <acws_ss@hotmail.com>
2024-08-26 18:10:46 -0300

    feat: malhorias

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-const express = require("express");   
+const express = require("express");
 const router = express.Router();
 const cors = require("cors");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-const foods = []
+const songs = [];
 
-const getfoods = router.get('/foods',(req, res)=>{
- res.status(200).json(foods)
+const createdsongs = router.post("/songs", (req, res) => {
+  const { title, description, status } = req.body;
 
-})
-
-const createfoods= router.post('/foods/creat',(req, res)=>{
-    const food = {
-    id: req.body.id,
+  const newsongs = {
+    id: uuidv4(),
     title: req.body.title,
     description: req.body.description,
-    status: req.body.status
-    }
+    status: req.body.status,
+  };
 
-    foods.push(food);
-    
-    res.status(201).json({ message: `food ${food.title} criado com sucesso!` })
-})
+  songs.push(newsongs);
+  res.status(201).json({
+    songs: songs,
+    message: `Songs Successfully created!`,
+  });
+});
 
+const getsongs = router.get("/songs", (req, res) => {
+  res.status(200).json(songs);
+});
 
-app.use(createfoods)
-app.use(getfoods)
+app.use(createdsongs);
+app.use(getsongs);
 
 app.listen(3333, console.log("Servidor rodando com sucesso"));
-
-
-    
-   
-
-
-
